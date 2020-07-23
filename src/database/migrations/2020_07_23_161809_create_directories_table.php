108b445332a0605519024c924360da7877e5c954
Richard Rohrig <richard.t.rohrig@gmail.com>
2020-07-23 21:29:43 +0200

    feature(nav) update Service Provider3

--- src/database/migrations/2020_07_23_161809_create_directories_table.php
+++ src/database/migrations/2020_07_23_161809_create_directories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Directories extends Migration
+class CreateDirectoriesTable extends Migration
 {
     /**
      * Run the migrations.
